c69ee1e50df49a87cf9a319562926e8e3edfe2a8
Sam Noedel <sam.noedel@changepoint.com>
2015-01-08 14:52:05 -0800

    Removing unused code

--- src/timeit.js
+++ src/timeit.js
@@ -2,20 +2,13 @@
 
     var sequence = require('./sequence');
 
-    var timeitConfig;
-    var timeit = function(name, sequence) {
+    function timeit(name, sequence) {
         if (!name) {
             throw new Error('name is required');
         }
         if (!sequence) {
             throw new Error('sequence is required');
         }
-
-        var pit = createPointInTime(type);
-        logPointInTime(pit);
-        var xhr = createXHR(pit);
-        var serializedPit = JSON.stringify(pit);
-        sendXHR(xhr, serializedPit);
     };
 
     timeit.prototype.sequence = function(name) {
@@ -24,39 +17,13 @@
         return seq;
     };
 
-    // TODO: Debounce this guy
-    function sendXHR(xhr, body) {
-        uri = 'http://localhost:8080/data';
-        xhr.open('post', uri, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(body);
-    }
-
-    function createPointInTime(name) {
+    function createPointInTime(name, sequenceID) {
         return {
             name: name,
-            timestamp: new Date().getTime()
+            timestamp: new Date().getTime(),
+            sequence: sequenceID
         };
     }
-
-    function createXHR(pit, success, error) {
-        // Create an XHR object
-        var xhr = new XMLHttpRequest();
-        // Attach the data
-
-        // Attach callbacks
-        if (success) {
-            xhr.onload = success;
-        }
-        if (error) {
-            xhr.onerror = error;
-        }
-
-        // return the XHR
-        return xhr;
-    }
-
-    configureTimeit();
 
     // Register as an AMD module if supported, otherwise create a global object
     if (typeof 'define' === 'function' && define.amd) {
