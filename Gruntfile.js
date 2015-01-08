9f7e7fe8efca4d77b434d0e79b2b23b45b8be196
Neil Breeden III <neil.breeden@changepoint.com>
2015-01-08 10:01:21 -0800

    Adding connect server

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,23 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         mocha: {
             src: ['test/**/*.html']
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: 'src/public',
+		    keepalive: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-mocha');
-
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    
     grunt.registerTask('default', [
-        'mocha'
+        'mocha',
+	'connect'
     ]);
 };
