3044438764a1fbf468d30df2f5f4bd1589c85194
Sam Noedel <sam.noedel@changepoint.com>
2015-01-08 14:45:14 -0800

    Adding README

---
+++ README.md
@@ -0,0 +1,13 @@
+# XHR Format
+
+``` json
+[
+    {
+        name: 'eventname',
+        data: { ... } || null,
+        sequence: 'someid',
+        timestamp: 1234567890
+    }
+    , ...
+ ]
+ ```
--- src/timeit.js
+++ src/timeit.js
@@ -2,23 +2,6 @@
 
     var sequence = require('./sequence');
 
-    /**
-     * XHR Format
-     * ----------
-     *
-     * This is a point in time (pit):
-     *
-     * [
-     *     {
-     *         name: 'eventname',
-     *         data: { ... } || null,
-     *         sequence: 'someid',
-     *         timestamp: 1234567890
-     *     }
-     *     , ...
-     *  ]
-     */
-
     var timeitConfig;
     var timeit = function(name, sequence) {
         if (!name) {
@@ -35,25 +18,11 @@
         sendXHR(xhr, serializedPit);
     };
 
-    timeit.prototype.configure = configureTimeit;
-
     timeit.prototype.sequence = function(name) {
         var seq = sequence();
         timeit(name, seq);
         return seq;
     };
-
-    function configureTimeit(opts) {
-        var defaultOptions = {
-            prot: 'http',
-            host: 'localhost',
-            port: undefined,
-            path: '/'
-        };
-
-        // Publish the settings globally to timeit
-        timeitConfig = defaultOptions;
-    }
 
     // TODO: Debounce this guy
     function sendXHR(xhr, body) {
@@ -63,20 +32,11 @@
         xhr.send(body);
     }
 
-    function createPointInTime(name, data) {
-        if (typeof data === 'undefined') {
-            data = null;
-        }
-
+    function createPointInTime(name) {
         return {
             name: name,
-            timestamp: new Date().getTime(),
-            data: data
+            timestamp: new Date().getTime()
         };
-    }
-
-    function logPointInTime(pit) {
-        console.log(pit.timestamp + ': ' + pit.name);
     }
 
     function createXHR(pit, success, error) {
